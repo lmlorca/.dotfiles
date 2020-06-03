30cabcc0b6fc14ad43c9a3989688f84e3656e20d
Luis M. Lorca <luismlorca@gmail.com>
2020-06-03 21:18:41 +0200

    Update

--- scripts/.scripts/reload-gtk.sh
+++ scripts/.scripts/reload-gtk.sh
@@ -3,9 +3,8 @@ gsettings set org.gnome.desktop.interface icon-theme '' &&
 gsettings set org.gnome.desktop.interface icon-theme 'flattrcolor' &&
 cd /home/lmlorca/.config/materia-theme/ &&
 ./install.sh -d /home/lmlorca/.themes --color dark --size compact --name wpg &&
-#./change_color.sh -t ~/.themes -o wpg-materia-change ~/.cache/wal/oomox &&
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact' &&
-gsettings set org.gnome.desktop.interface gtk-theme 'wpg-dark-compact' &&
+gsettings set org.gnome.desktop.interface gtk-theme 'wpg-dark-compact'
 ./render-assets.sh gtk2-dark &&
 ./install.sh -d /home/lmlorca/.themes --color dark --size compact --name wpg &&
 sleep 1 &&
