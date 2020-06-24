0ea45d0c5b4773faaaaa7812ada3d6dc07004d93
Luis M. Lorca <luismlorca@gmail.com>
2020-06-25 00:59:15 +0200

    stuff

--- scripts/.scripts/switchfont.sh
+++ scripts/.scripts/switchfont.sh
@@ -5,8 +5,8 @@ CURRENT=$(grep -i -m 1 'family: ' $HOME/.config/alacritty/alacritty.yml | awk '{
 if [[ $CURRENT == 'JetBrainsMono' ]];
 then
   sed -i 's/JetBrainsMono Nerd Font/CentSchbook Mono BT/g' $ALACRITTY
-  sed -i 's/size: 10.0/size: 12.0/g' $ALACRITTY
+  #sed -i 's/size: 10.0/size: 12.0/g' $ALACRITTY
 else
   sed -i 's/CentSchbook Mono BT/JetBrainsMono Nerd Font/g' $ALACRITTY
-  sed -i 's/size: 12.0/size: 10.0/g' $ALACRITTY
+  #sed -i 's/size: 12.0/size: 10.0/g' $ALACRITTY
 fi
