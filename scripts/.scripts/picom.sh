bc2b00ae4c469e3214dcd0ea49cb10064097fde5
L. M. Lorca <luismlorcq@gmail.com>
2021-02-04 21:08:07 -0400

    .

--- scripts/.scripts/picom.sh
+++ scripts/.scripts/picom.sh
@@ -6,7 +6,7 @@ killall -q picom
 # Wait until the processes have been shut down
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
-picom --experimental-backends --blur-method dual_kawase -m 0.85 &
-# picom &
+# picom --experimental-backends --blur-method dual_kawase -m 0.85 &
+picom &
 
 echo "Picom launched..."
